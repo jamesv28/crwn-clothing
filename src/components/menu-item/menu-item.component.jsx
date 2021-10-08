f5b68b342db67525de2993faabf242e7eb0eb8e3
James Volmert <james.volmert@gmail.com>
2021-10-07 19:51:55 -0600

    added hompage, directory and menu item components

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './menu-item.styles.scss';
+
+const MenuItem = ({title, imageUrl,size}) => (
+    <div 
+        style={{backgroundImage: `url(${imageUrl})`}} 
+        className={`${size} menu-item`}
+    >
+        <div className="content">
+            <div className="title">{title}</div>
+            <div className="subtitle">Shop Now</div>
+        </div>
+    </div>
+)
+
+export default MenuItem;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+import Directory from '../../components/directory/director.component';
+
+const Homepage = () => (
+    <main className="homepage">
+        <Directory />
+    </main>
+)
+
+export default Homepage;
