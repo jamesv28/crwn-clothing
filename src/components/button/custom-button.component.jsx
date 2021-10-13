b79554cf0cbef55f223f6d752ef06aa89ad74ea8
James Volmert <james.volmert@gmail.com>
2021-10-13 16:37:28 -0600

    forgot to name the button class name correctly

--- src/components/button/custom-button.component.jsx
+++ src/components/button/custom-button.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 const CustomButton = ({children, ...otherProps}) => (
-    <button className="cutom-button" {...otherProps}> 
+    <button className="custom-button" {...otherProps}> 
         {children}   
     </button>
 )
