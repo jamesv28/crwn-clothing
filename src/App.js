03a72f899ea7f74e0dee35dde6b6d4d8828aafe2
James Volmert <james.volmert@gmail.com>
2021-10-18 17:38:59 -0600

    added action and reducers for signing in and signing out

--- src/App.js
+++ src/App.js
@@ -7,36 +7,33 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-siign-u
 import NotFound from './pages/not-found/not-found.page';
 
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import {connect} from 'react-redux';
+import {setCurrentUser} from './redux/user/user.action';
 
 import './App.css';
 
 class App extends Component{
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
+            })
           });
 
-        });
+
       }
 
-      this.setState({ currentUser: userAuth });
+      setCurrentUser(userAuth);
     });
   }
 
@@ -45,10 +42,9 @@ class App extends Component{
   }
 
   render() {
-    const {currentUser} = this.state;
     return (
       <>
-        <Header currentUser={currentUser} />
+        <Header />
         <main>
           <Switch>
             <Route exact path="/" component={Homepage} />
@@ -61,5 +57,11 @@ class App extends Component{
     );
   }
 }
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
 
-export default App;
+export default connect(
+  null,
+  mapDispatchToProps
+  )(App);
