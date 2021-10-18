d501293fce2c28d523c5797f651a0031c9600977
James Volmert <james.volmert@gmail.com>
2021-10-18 17:54:04 -0600

    updated our app to redirect to main page if logged in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route, Switch } from 'react-router-dom';
+import {Route, Switch, Redirect } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
@@ -29,8 +29,6 @@ class App extends Component{
               ...snapShot.data()
             })
           });
-
-
       }
 
       setCurrentUser(userAuth);
@@ -49,7 +47,7 @@ class App extends Component{
           <Switch>
             <Route exact path="/" component={Homepage} />
             <Route path="/shop"  component={ShopPage} />
-            <Route path="/signin" component={SignInAndSignUpPage} />
+            <Route exact path="/signin" render={() => this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />} />
             <Route component={NotFound} />
           </Switch>
         </main>
@@ -57,11 +55,16 @@ class App extends Component{
     );
   }
 }
+
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
   )(App);
